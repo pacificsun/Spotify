8e8df7dcb09a8fca296514138837f1187f47e489
Suraj Thapa <stsurajst@gmail.com>
2020-11-28 23:17:31 +0545

    album header

---
+++ components/AlbumHeader/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumHeader = (props: AlbumHeaderProps) => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default AlbumHeader;
